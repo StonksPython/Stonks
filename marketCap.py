98f1be36348edaa0a5410689e9095357934d629f
Dev Patel <2021dpatel@gmail.com>
2020-04-17 16:47:08 +0000

    AlphaVantage interfacing added

--- marketCap.py
+++ marketCap.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from numpy import sqrt,mean,log,diff
 from pandas_datareader import data
-import quandl
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import sys
@@ -11,9 +9,8 @@ if sys.warnoptions:
     warnings.simplefilter("ignore")
 #Return Stock Volatility
 def marketCap(name):
-    name = name[5:]
     marketCap = data.get_quote_yahoo(name)['marketCap']
     temp =  marketCap[name]
     return temp
-i = marketCap('WIKI/GOOGL')
+i = marketCap('GOOGL')
 print(i)
--- volatility.py
+++ volatility.py
@@ -1,13 +1,13 @@
-#Code Heavily Based of this site: https://www.quantconnect.com/tutorials/introduction-to-options/historical-volatility-and-implied-volatility
+#Code is Based of this site: https://www.quantconnect.com/tutorials/introduction-to-options/historical-volatility-and-implied-volatility
 import pandas as pd
 from numpy import sqrt,mean,log,diff
 import quandl
 quandl.ApiConfig.api_key = 'NxTUTAQswbKs5ybBbwfK'
 #Historical Volitiliy is based on past performance - how much stock varies from Market
 def historicalVolatility(name):
-    stock_table = quandl.get(name)
+    df = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
     # use the daily data of Google(NASDAQ: GOOG) from 01/2016 to 08/2016
-    close = stock_table['2019-01':'2020-04']['Adj. Close']
+    close = df['close']
     r = diff(log(close))
     r_mean = mean(r)
     diff_square = [(r[i]-r_mean)**2 for i in range(0,len(r))]
@@ -15,4 +15,5 @@ def historicalVolatility(name):
     vol = std*sqrt(252)
     return vol
 
-#Beta Coefficient Volatility
+i = historicalVolatility('IBM')
+print(i)
