1a39572e1dfcd61a7725ffb1e7fc6fea2692e901
Dev Patel <2021dpatel@gmail.com>
2020-04-18 16:33:07 +0000

    updated sharpeRatio

--- sharpRatio.py
+++ sharpRatio.py
@@ -5,11 +5,13 @@ import pandas as pd
 import datetime as dt
 from fbprophet import Prophet
 import numpy as np
-fb = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'FB' +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
-ibm = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'IBM' +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
-amzn = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'AMZN' +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
-google = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'GOOGL' +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
-apple = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'AAPL' +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
+fb = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'FB' +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
+ibm = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'IBM' +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
+amzn = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'AMZN' +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
+google = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'GOOGL' +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
+apple = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 'AAPL' +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
 for df in (fb, ibm, amzn, google, apple):
-    df['Normalized Return'] = df['close']/df.iloc[-1]['close']
-    
+    df = df.rename(columns={"timestamp":"Date"})
+    df = df.setIndex(df['Date'])
+    df = df.sort_index()
+    df['Normalized Return'] = df['close']/df.iloc[0]['close']
