78197d19148c2de9d6bfb3ba5484427ffddce158
Dev Patel <2021dpatel@gmail.com>
2020-04-18 21:13:10 +0000

    added multiprocessing

--- predictionAlphaVantage.py
+++ predictionAlphaVantage.py
@@ -5,9 +5,13 @@ import pandas as pd
 import datetime as dt
 from fbprophet import Prophet
 import numpy as np
+from tqdm import tqdm
+import time
 #Predicting Stock Price with Prophet
-def prophetPredict(name):
+def get_dataframe(name):
     df = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
+    return df
+def run_prophet(df):
     df = df.rename(columns={"timestamp": "Date"})
     fig3 = df.plot(y='high')
     fig3.figure.savefig('/home/dev/Stonks/preIndexReset.png')
@@ -24,5 +28,21 @@ def prophetPredict(name):
     fig1.savefig('/home/dev/Stonks/postProphet.png')
     i = forecast[['yhat']].iloc[-1]
     return i['yhat']
-y = prophetPredict('AAPL')
-print(y)
+def get_series(names):
+    series = []
+    for name in names:
+        df = get_dataframe(name)
+        series.append(df)
+    return series
+#main is here
+names = ['AAPL', 'GOOGL', 'FB', 'IBM', 'AMZN']
+series = get_series(names)
+
+#multiprocessing implemented - https://medium.com/spikelab/forecasting-multiples-time-series-using-prophet-in-parallel-2515abd1a245
+from multiprocessing import Pool, cpu_count
+start_time = time.time()
+p = Pool(cpu_count())
+predictions = list(tqdm(p.imap(run_prophet, series), total=len(series)))
+p.close()
+p.join()
+print("--- %s seconds ---" % (time.time() - start_time))
