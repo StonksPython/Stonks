9ef80a5d394d57165880e5a5f063fc25a77a3ce7
moonbeam87 <2021dpatel@gmail.com>
2020-04-24 16:56:46 +0000

    monte carlo

--- predictionAlphaVantage.py
+++ predictionAlphaVantage.py
@@ -9,23 +9,23 @@ from tqdm import tqdm
 import time
 #Predicting Stock Price with Prophet
 def get_dataframe(name):
-    df = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=WCXVE7BAD668SJHL&datatype=csv&outputsize=full')
+    df = pd.read_csv('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=WCXVE7BAD668SJHL&datatype=csv')
     return df
 def run_prophet(df):
     df = df.rename(columns={"timestamp": "Date"})
-    fig3 = df.plot(y='high')
-    fig3.figure.savefig('/home/dev/Stonks/preIndexReset.png')
+    #fig3 = df.plot(y='high')
+    #fig3.figure.savefig('/home/homuser/Stonks/preIndexReset.png')
     df = df.reset_index(0)
     df = df.drop(columns=['open', 'low', 'close', 'volume'])
     df = df.rename(columns={"Date": "ds", "high": "y"})
-    fig = df.plot(x='ds', y='y')
-    fig.figure.savefig('/home/dev/Stonks/preProphet.png')
+    #fig = df.plot(x='ds', y='y')
+    #fig.figure.savefig('/home/homeuser/Stonks/preProphet.png')
     m = Prophet()
     m.fit(df)
     future = m.make_future_dataframe(periods=5)
     forecast = m.predict(future)
-    fig1 = m.plot(forecast)
-    fig1.savefig('/home/dev/Stonks/postProphet.png')
+    #fig1 = m.plot(forecast)
+    #fig1.savefig('/home/homeuser/Stonks/postProphet.png')
     i = forecast[['yhat']].iloc[-1]
     return i['yhat']
 def get_series(names):
