bfd8bccdf5c85d5764cb78298c17537923ff7b39
moonbeam87 <2021dpatel@gmail.com>
2020-05-01 12:02:53 -0400

    re-organized code, added proper method structure

--- predictionAlphaVantage.py
+++ predictionAlphaVantage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pandas as pd
 from pandas_datareader import data
 import matplotlib.pyplot as plt
@@ -8,8 +9,9 @@ import numpy as np
 from tqdm import tqdm
 import time
 import requests
+from multiprocessing import Pool, cpu_count
 #Predicting Stock Price with Prophet
-predictedPrices = {}
+
 def get_dataframe(name):
     
     df = pd.read_csv(('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=JA1VCTFBG7378ZB7&datatype=csv'))
@@ -30,7 +32,6 @@ def run_prophet(df):
     #fig1 = m.plot(forecast)
     #fig1.savefig('/home/homeuser/Stonks/postProphet.png')
     i = forecast[['yhat']].iloc[-1]
-    predictedPrices['name'].append(i['yhat'])
     return i['yhat']
 def get_series(names):
     series = []
@@ -38,15 +39,19 @@ def get_series(names):
         df = get_dataframe(name)
         series.append(df)
     return series
-#main is here
-names = ['AAPL', 'GOOGL', 'FB', 'IBM', 'AMZN']
-series = get_series(names)
 
 #multiprocessing implemented - https://medium.com/spikelab/forecasting-multiples-time-series-using-prophet-in-parallel-2515abd1a245
-from multiprocessing import Pool, cpu_count
-start_time = time.time()
-p = Pool(cpu_count())
-predictions = list(tqdm(p.imap(run_prophet, series), total=len(series)))
-p.close()
-p.join()
-print("--- %s seconds ---" % (time.time() - start_time))
+def predictedPrices(names):
+    series = get_series(names)
+    start_time = time.time()
+    p = Pool(cpu_count())
+    predictions = list(tqdm(p.imap(run_prophet, series), total=len(series)))
+    predictedPrices = {}
+    count = 0
+    for name in names:
+        predictedPrices[name].append(predictions[count])
+        count+=1
+    p.join()
+    print(predictions)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    return predictedPrices
