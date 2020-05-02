90ec0ca70b87dd773b1afa3e0f75849e27f4f79a
moonbeam87 <2021dpatel@gmail.com>
2020-05-02 13:15:45 -0400

    updated pointChange

--- pointChange.py
+++ pointChange.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python3
 import pandas as pd
-from numpy import sqrt,mean,log,diff
-import quandl
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
-one_yr_ago = (datetime.today()- relativedelta(years=1)).strftime('%Y-%m')
-six_months_ago = (datetime.today()- relativedelta(months=6)).strftime('%Y-%m')
-
-print(one_yr_ago)
-print(six_months_ago)
-quandl.ApiConfig.api_key = 'NxTUTAQswbKs5ybBbwfK'
+def get_dataframe(name):
+    
+    df = pd.read_csv(('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + name +'&apikey=JA1VCTFBG7378ZB7&datatype=csv'))
+    return df
 
 def allTimePointChange(name):
-    stock_table = quandl.get(name)
-    close = stock_table[['Adj. Close']]
-    first = close['Adj. Close'].iloc[0]
-    last = close['Adj. Close'].iloc[-1]
-    percentageChange = last/first * 100
-    print(percentageChange)
-    return percentageChange
-oneYearPointTrend('WIKI/GOOGL')
+    df = get_dataframe(name)
+    last = df['close'][-1]
+    first = df['close'][0]
+    return first/last * 100
